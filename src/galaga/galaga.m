8c33785a8dd3d49bf66c4866152186fe61953ddc
Taten <knight.taten@gmail.com>
2022-01-22 14:36:10 -0700

    Comments and parfor loops

--- src/galaga/galaga.m
+++ src/galaga/galaga.m
@@ -15,30 +15,36 @@ mapSize = 40;
 
 xPos = gpuArray([0 1 2]);
 yPos = gpuArray([0 1 0]);
-
+% Contains the x values for the first row of enemies
+% # enemies x 3 array, where each of the three values in a row
+% represents the horizontal position of each of the points
+% on the triangular enemies
 enemyXStart = ...
     gpuArray([-5 -4 -3;
     -1 0 1;
     3 4 5]);
-
+% The height at which enemies will ALWAYS start
 enemyYStart = ...
     gpuArray([mapSize/2 - 1, mapSize/2 - 2, mapSize/2 - 1]);
-
+% Start the first row at the correct place
 enemyYValues = enemyYStart;
-
+% Set the horizontal movement speed of the enemies
+% Each entry represents the speed of a single row of enemies
 enemyRowMove = gpuArray([rand]);
-
+% Contains the x values for each row of enemies
+% One key represents a row of enemies, and the value is:
+% # enemies x 3 array, where each of the three values in a row
+% represents the horizontal position of each of the points
+% on the triangular enemies
 rowValues = containers.Map({1}, {enemyXStart});
-
+% Keeps track of the number of rows
 totalRows = 1;
-
+% TODO: Keeps track of friendly bullet positions
 bulletsX = gpuArray([]);
 bulletsY = gpuArray([]);
-
+% Initial movement directions of the player
 xMove = .1;
 yMove = 0;
-
-turn = 1;
 
 scatter(xPos,yPos,'filled','b')
 hold on
@@ -49,6 +55,20 @@ set(gcf,'position',dim);
 colors = ['r' 'y' 'k' 'o'];
 
 set(gcf,'KeyPressFcn',@stroke)
+
+% Try to implement the parfor loop for collision detection. Not sure that
+% this is even possible. Collision steps:
+% 1. Get row values from object
+% 2. Check if leftmost or righmost points have overlapped boundaries
+%   - Maybe there is a way to vectorize this check by moving the boundaries
+%       into an array and checking the entire array for being greater than
+%       the right boundary or less than the left boundary. Maybe ignore
+%       step 3 if this is the case and move down on any boundary impact.
+%       ** Move all points and check if array > right || array < left 
+% 3. If yes, check if the overlapping dot was in the bottom-most row
+%   - Need to update bottom-most row in the future when a row is gone
+
+
 while xPos(1) > -mapSize/2 & ... 
         xPos(end) < mapSize/2
     
@@ -79,14 +99,12 @@ while xPos(1) > -mapSize/2 & ...
         if rightValue + enemyRowMove(i) > mapSize / 2 |...
                 leftValue + enemyRowMove(i) < - mapSize / 2
             enemyRowMove(i) = -(enemyRowMove(i) + .1);
-            if i == 1
-                enemyYValues = enemyYValues - 1;
-                if enemyYValues(end) - 1 < mapSize - 6
-                    totalRows = totalRows + 1;
-                    enemyYValues = gpuArray([enemyYValues; enemyYStart]);
-                    rowValues(totalRows) = enemyXStart;
-                    enemyRowMove = gpuArray([enemyRowMove rand]);
-                end
+                enemyYValues = enemyYValues - .2;
+            if enemyYValues(end) - 1 < mapSize / 2 - 3
+                totalRows = totalRows + 1;
+                enemyYValues = gpuArray([enemyYValues; enemyYStart]);
+                rowValues(totalRows) = enemyXStart;
+                enemyRowMove = gpuArray([enemyRowMove rand]);
             end
         end
         rowValues(i) = rowValues(i) + enemyRowMove(i);
